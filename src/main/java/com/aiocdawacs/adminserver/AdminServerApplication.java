15a82cde4dc89e7e5c3d5f1931d6782d9a236add
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-12 19:09:36 +0530

    spring seucrity csrf isue

--- src/main/java/com/aiocdawacs/adminserver/AdminServerApplication.java
+++ src/main/java/com/aiocdawacs/adminserver/AdminServerApplication.java
@@ -2,9 +2,11 @@ package com.aiocdawacs.adminserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+@EnableWebSecurity 
 @SpringBootApplication
 @EnableAdminServer
 public class AdminServerApplication {
